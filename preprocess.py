8af9d97cac356d739a7060bfe6e9d7f0792fa01a
weiber23727698 <b09902123@ntu.edu.tw>
2023-05-17 10:18:06 +0800

    fix the order

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from ordered_set import OrderedSet
 
 def normalize(X: np.ndarray) -> np.ndarray:
     X = (X - min(X)) / (max(X) - min(X))
@@ -19,52 +20,57 @@ def make_X(df) -> np.ndarray:
                 x[i].append(val)
     return np.array(x)
 
-def load_data(file):
-    data = pd.read_csv(file)
-    for key in data.keys():
+def write_file(filename, df):
+    file = open(filename, "w")
+    for i, data in enumerate(df):
+        assert(len(data) == 43)
+        file.write(f"{data[-1]}") # attack
+        for j in range(len(data)-1):
+            file.write(f" {j+1}:{data[j]}")
+        file.write("\n")
+    file.close()
+
+def load_data():
+    ## training train_data
+    train_data = pd.read_csv("./data/UNSW_NB15_training-set.csv")
+    for key in train_data.keys():
         if key not in no_normalize:
-            data[key] = normalize(data[key])
+            train_data[key] = normalize(train_data[key])
+    print("training normalization complete")
+    ## testing data
+    test_data = pd.read_csv("./data/UNSW_NB15_testing-set.csv")
+    for key in test_data.keys():
+        if key not in no_normalize:
+            test_data[key] = normalize(test_data[key])
+    print("testing normalization complete")
     ## proto one hot
-    proto = list(set(data['proto']))
-    data['proto'] = [proto.index(i) for i in data['proto']]
+    proto = list(OrderedSet(train_data['proto']).union(OrderedSet(test_data['proto'])))
+    train_data['proto'] = [proto.index(i) for i in train_data['proto']]
+    test_data['proto'] = [proto.index(i) for i in test_data['proto']]
     ## service one hot
-    service = list(set(data['service']))
-    data['service'] = [service.index(i) for i in data['service']]
+    service = list(OrderedSet(train_data['service']).union(OrderedSet(test_data['service'])))
+    train_data['service'] = [service.index(i) for i in train_data['service']]
+    test_data['service'] = [service.index(i) for i in test_data['service']]
     ## attack one hot
-    attack = list(set(data['attack_cat']))
-    print(attack)
-
-    data['attack_cat'] = [attack.index(i) for i in data['attack_cat']]
+    attack = list(OrderedSet(train_data['attack_cat']).union(OrderedSet(test_data['attack_cat'])))
+    train_data['attack_cat'] = [attack.index(i) for i in train_data['attack_cat']]
+    test_data['attack_cat'] = [attack.index(i) for i in test_data['attack_cat']]
     ## state one hot
-    state = list(set(data['state']))
-    data['state'] = [state.index(i) for i in data['state']]
-
-    data = make_X(data)
-    return data
+    state = list(OrderedSet(train_data['state']).union(OrderedSet(test_data['state'])))
+    train_data['state'] = [state.index(i) for i in train_data['state']]
+    test_data['state'] = [state.index(i) for i in test_data['state']]
+    print("one hot complete")
+    ## write to training file
+    train_data = make_X(train_data)
+    write_file("train", train_data)
+    print("write to the training file")    
+    ## write to testing file
+    test_data = make_X(test_data)
+    write_file("test", test_data)
+    print("write to the testing file")
 
 def main():
-    ## load train data
-    train = load_data("./data/UNSW_NB15_training-set.csv")
-    train_file = open("train", "w")
-    for i, data in enumerate(train):
-        assert(len(data) == 43)
-        train_file.write(f"{data[-1]}") # attack
-        for j in range(len(data)-1):
-            train_file.write(f" {j+1}:{data[j]}")
-        train_file.write("\n")
-    train_file.close()
-    print("training data loaded")
-    ## load test data
-    test = load_data("./data/UNSW_NB15_testing-set.csv")
-    test_file = open("test", "w")
-    for i, data in enumerate(test):
-        assert(len(data) == 43)
-        test_file.write(f"{data[-1]}") # attack
-        for j in range(len(data)-1):
-            test_file.write(f" {j+1}:{data[j]}")
-        test_file.write("\n")
-    test_file.close()
-    print("testing data loaded")
+    load_data()
 
 if __name__ == "__main__":
     main()
