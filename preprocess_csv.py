d42735c919a9b6e7ec54a30fdda2cf2db1d863fd
blackjack061524 <blackjack061524@gmail.com>
2023-05-23 02:13:29 +0800

    attack_cat to multiclass label

--- preprocess_csv.py
+++ preprocess_csv.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+import json
 import pandas as pd
 
-def preprocessing(data, split):
+def preprocess(data, output_path):
     data = data.dropna()
     # min-max normalization
     no_normalize = ['id', 'proto', 'service', 'attack_cat', 'state', 'label']
@@ -9,11 +11,19 @@ def preprocessing(data, split):
             continue
         data[column] = (data[column] - data[column].min()) / (data[column].max() - data[column].min())
     # one-hot encoding
-    data = pd.get_dummies(data)
-    data.to_csv(f'{split}.csv', index=False)
+    cat_columns = ['proto', 'service', 'state']
+    one_hot = pd.get_dummies(data[cat_columns])
+    data = data.drop(cat_columns, axis=1)
+    data = data.join(one_hot)
+    data.to_csv(output_path, index=False)
 
 if __name__ == "__main__":
-    train_data = pd.read_csv('./data/UNSW_NB15_training-set.csv')
-    preprocessing(train_data, 'train')
-    test_data = pd.read_csv("./data/UNSW_NB15_testing-set.csv")
-    preprocessing(test_data, 'test')
+    preprocess_dir = Path('./preprocess/')
+    preprocess_dir.mkdir(parents=True, exist_ok=True)
+    # attack_cat to multiclass label
+    attack2idx_path = preprocess_dir / 'attack2idx.json'
+    attack2idx = json.loads(attack2idx_path.read_text())
+    for split in ['train', 'test']:
+        data = pd.read_csv(f'./data/UNSW_NB15_{split}ing-set.csv')
+        data['attack_cat'] = [attack2idx[attack_cat] for attack_cat in data['attack_cat']]
+        preprocess(data, preprocess_dir / f'{split}.csv')
